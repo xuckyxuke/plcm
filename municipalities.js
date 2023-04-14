d109425ef345610df8bef53d02ba272d110e5c5d
xuckyxuke <101798201+xuckyxuke@users.noreply.github.com>
2023-04-14 10:56:22 +0200

    Update municipalities.js

--- municipalities.js
+++ municipalities.js
@@ -2,6 +2,16 @@ export { municipalities };
 
 const municipalities = [
     {
+        name: 'Getafe',
+        bocmDate: '2023-01-26',
+        coaImg: 'https://upload.wikimedia.org/wikipedia/commons/2/26/Escudo_de_Getafe.svg',
+        vacancies: 19,
+        links: [
+            { name: 'Portal', url: 'https://sede.getafe.es/portalGetafe/sede/se_contenedor1.jsp?seccion=s_fdes_d4_v4.jsp&contenido=22595&nivel=1400&tipo=6&codResi=1&language=es' },
+        ],
+        tests: ['P', 'C', 'F'],
+    },
+    {
         name: 'Coslada',
         bocmDate: '2022-08-24',
         coaImg: 'https://upload.wikimedia.org/wikipedia/commons/9/94/Escudo_de_Coslada.svg',
@@ -17,7 +27,7 @@ const municipalities = [
         coaImg: 'https://upload.wikimedia.org/wikipedia/commons/4/45/Escudo_de_Majadahonda.svg',
         vacancies: 5,
         links: [
-            { name: '', url: '' },
+            { name: 'Portal', url: 'https://www.majadahonda.org/oposicion-libre/-/asset_publisher/hT4RJ49u8NeC/content/-20220721-plazas-de-policia-local' },
         ],
         tests: [],
     },
@@ -27,7 +37,50 @@ const municipalities = [
         coaImg: 'https://upload.wikimedia.org/wikipedia/commons/4/46/Escudo_de_Fuenlabrada.svg',
         vacancies: 8,
         links: [
-            { name: '', url: '' },
+            { name: 'Portal', url: 'https://convocapub.ayto-fuenlabrada.es/ofertaEmpleoPublicoCategoriaPublico/listPublicacionesConCategoria?categoria.id=242' },
+        ],
+        tests: [],
+    },
+    {
+        name: 'San Lorenzo de El Escorial',
+        bocmDate: '2022-07-27',
+        coaImg: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Escudo_de_San_Lorenzo_del_Escorial.svg',
+        vacancies: 4,
+        links: [
+            { name: 'Portal', url: 'https://transparencia.aytosanlorenzo.es/ayuntamiento/empleados-publicos/oferta-publica-de-empleo/' },
+            { name: 'Tablon de Anuncios', url: 'https://sede.aytosanlorenzo.es/GDCarpetaCiudadano/Tablon.do?action=verAnuncios&tipoTablon=1' },
+        ],
+        tests: [],
+    },
+    {
+        name: 'Torrelodones',
+        bocmDate: '2022-07-27',
+        coaImg: 'https://upload.wikimedia.org/wikipedia/commons/a/ad/Escudo_de_Torrelodones.svg',
+        vacancies: 6,
+        links: [
+            { name: 'Portal', url: 'https://www.torrelodones.es/empleo/empleo-publico/oferta-de-empleo-publico/8692-policia-local-concurso-oposicion-libre' },
+            { name: 'Tablon de Anuncios', url: 'https://sede.torrelodones.es/eAdmin/Tablon.do?action=verAnuncios&tipoTablon=1' },
+        ],
+        tests: [],
+    },
+    {
+        name: 'Madrid',
+        bocmDate: '2022-06-07',
+        coaImg: 'https://upload.wikimedia.org/wikipedia/commons/6/67/Escudo_de_Madrid.svg',
+        vacancies: 189,
+        links: [
+            { name: 'Portal', url: 'https://www.madrid.es/portales/munimadrid/es/Inicio/Emergencias-y-seguridad/Policia-Municipal/Policia-del-Cuerpo-de-Policia-Municipal-del-Ayuntamiento-de-Madrid/?vgnextfmt=default&vgnextoid=975fe92dfa341810VgnVCM1000001d4a900aRCRD&vgnextchannel=3926bb21278fe410VgnVCM1000000b205a0aRCRD' },
+            { name: 'Tablon de Anuncios', url: 'https://sede.madrid.es/sites/v/index.jsp?vgnextoid=5527814231ede410VgnVCM1000000b205a0aRCRD&buscar=true&buscador.edictos.texto=&filterTipo=&filtroReferencia=&filterCentro=Area_de_Gobierno_de_Portavoz_Seguridad_y_Emergencias&filterCentroExterno=' },
+        ],
+        tests: [],
+    },
+    {
+        name: 'Valdemoro',
+        bocmDate: '2021-12-20',
+        coaImg: 'https://upload.wikimedia.org/wikipedia/commons/7/76/Escudo_de_Valdemoro.svg',
+        vacancies: 4,
+        links: [
+            { name: 'Portal', url: 'https://sede.valdemoro.es/ope/-/asset_publisher/k9Qp8xyiRPZT/content/cuatro-plazas-de-policia-del-cuerpo-de-policia-local;jsessionid=F9C8C14DBD17520D5BA0D1DA1F661A3A?redirect=https%3A%2F%2Fsede.valdemoro.es%2Fope%3Bjsessionid%3DF9C8C14DBD17520D5BA0D1DA1F661A3A%3Fp_p_id%3D101_INSTANCE_k9Qp8xyiRPZT%26p_p_lifecycle%3D0%26p_p_state%3Dnormal%26p_p_mode%3Dview%26p_p_col_id%3Dcolumn-1%26p_p_col_pos%3D1%26p_p_col_count%3D2' },
         ],
         tests: [],
     },
