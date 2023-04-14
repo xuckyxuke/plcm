fa07a33e6445409d253329a13c358c7e917d585c
xuckyxuke <101798201+xuckyxuke@users.noreply.github.com>
2023-04-14 09:53:41 +0200

    Update municipalities.js

--- municipalities.js
+++ municipalities.js
@@ -2,6 +2,46 @@ export { municipalities };
 
 const municipalities = [
     {
-        "id": 1,
+        name: 'Coslada',
+        bocmDate: '2022-08-24',
+        coaImg: '',
+        vacancies: 8,
+        links: [
+            { name: '', url: '' },
+        ],
+        tests: ['P', 'C', 'F'],
+    },
+    {
+        name: 'Majadahonda',
+        bocmDate: '2022-08-16',
+        coaImg: '',
+        vacancies: 5,
+        links: [
+            { name: '', url: '' },
+        ],
+        tests: [],
+    },
+    {
+        name: 'Fuenlabrada',
+        bocmDate: '2022-07-29',
+        coaImg: '',
+        vacancies: 8,
+        links: [
+            { name: '', url: '' },
+        ],
+        tests: [],
     },
 ];
+
+const other = [
+    {
+        name: '',
+        bocmDate: '',
+        coaImg: '',
+        vacancies: 0,
+        links: [
+            { name: '', url: '' },
+        ],
+        tests: [],
+    },
+]
